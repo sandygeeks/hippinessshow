bd79a6b1b82c0fa95e4c659708115d2ec5ea96ca
Sanjit Raj Shakya <sanjit.shakya@nayauser.com>
2024-12-19 23:53:24 +0545

    songs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { Season } from './season';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Season, Song } from './season';
 import { NgFor, NgIf } from '@angular/common';
 import { getSeasons } from './service/song.service';
 
@@ -11,9 +11,15 @@ import { getSeasons } from './service/song.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public OnClickSong(season: Season, song: Song) {
+    const link = `/season/${season.label}/song/${song.id}`;
+    this._router.navigate([link]);
+    this.toggleSidebar();
+  }
   title = 'HippinessShow';
   seasons: Season[] = [];
   sidebarOpen = false;
+  constructor(private _router: Router) { }
 
   async ngOnInit() {
     this.seasons = await getSeasons();
